9582b46f4a46a3f0bffce07d663d6862c1fb92d4
prasanthmj <prasanthmj@gmail.com>
2023-01-30 16:35:04 +0530

    gets parameter values used

--- src/identifiers.ts
+++ src/identifiers.ts
@@ -82,3 +82,37 @@ function getValidIdentifiers(code: string): string[] {
     return variables
   }
   
+
+  export function extractParameters(code: string): string[] {
+    const params = code.match(/\(([^)]*)\)/)![1];
+    return params.split(',').map(p => p.trim());
+  }
+  
+
+  export function getParametersUsed(code:string){
+
+    // Remove single-line comments
+    code = code.replace(/\/\/.*/g, "");
+  
+    // Remove multi-line comments
+    code = code.replace(/\/\*[\s\S]*?\*\//g, "");
+
+     const params = extractParameters(code)
+     if(params.length <= 0 ){
+        return false
+     }
+
+     const strregex = `\\b(${params[0]})\\.([a-zA-Z_$][\\w$]*)\\b`
+
+     const pattern = new RegExp(strregex, "g");
+
+     const matches =  code.match(pattern)
+
+     const fullvar = Array.from(new Set(matches));
+
+     const variables = fullvar.map((m) => m.split(".")[1])
+     
+    return variables
+  }
+
+  
--- tests/identifiers.test.ts
+++ tests/identifiers.test.ts
@@ -1,5 +1,5 @@
 
-import {getIdentifiersFromFunction} from "../src/identifiers"
+import {getIdentifiersFromFunction, extractParameters, getParametersUsed} from "../src/identifiers"
 
 test("get identifiers in a function",()=>{
 
@@ -52,4 +52,79 @@ test("get identifiers with conditions and while",()=>{
     expect(ids.includes("function")).toBe(false)
 
     expect(ids.includes("items")).toBe(true)
+})
+
+test("get function parameters",()=>{
+
+    const fn = (formdata:any)=>{
+        /**
+         * this function should be called like this
+         * function(fkkk)
+         * 
+         */
+        return formdata.qty*10
+    }
+
+
+    const params = extractParameters(fn.toString())
+
+    console.log("params ", params)
+})
+
+test("get parameters_used",()=>{
+
+    const fn = (formdata:any)=>{
+        /**
+         * this function should be called like this
+         * function(fkkk)
+         * formdata.invalid
+         * 
+         */
+        //formdata.invalid
+        let res = formdata.items[0].qty * formdata.items[0].price + formdata.global
+        return res
+    }
+
+
+    const variables = getParametersUsed(fn.toString())
+
+    expect(variables).not.toBe(false)
+
+    variables && expect(variables.includes("invalid")).toBe(false)
+
+    variables && expect(variables.includes("res")).toBe(false)
+
+    variables && expect(variables.includes("items")).toBe(true)
+
+    variables && expect(variables.includes("global")).toBe(true)
+})
+
+test("get parameters_used different format",()=>{
+
+    function another(ffd:any){
+        if(ffd.isCorrect){
+            return ffd.firstName + ffd.lastName
+        }
+        // ffd.invalidmember
+        /** ffd.another */
+        return ffd.items[0].qty * ffd.items[0].price 
+    }
+
+
+    const variables = getParametersUsed(another.toString())
+
+    expect(variables).not.toBe(false)
+
+    variables && expect(variables.includes("invalidmember")).toBe(false)
+
+    variables && expect(variables.includes("another")).toBe(false)
+
+    variables && expect(variables.includes("items")).toBe(true)
+
+    variables && expect(variables.includes("firstName")).toBe(true)
+
+    variables && expect(variables.includes("isCorrect")).toBe(true)
+
+    variables && expect(variables.includes("lastName")).toBe(true)
+
 })
